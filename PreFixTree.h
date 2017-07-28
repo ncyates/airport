7fdd850dad06d13c24ef1d8b14eff423a9a8367f
nick <ncyates@users.noreply.github.com>
2017-07-28 16:05:24 -0700

    upload local files to remote

---
+++ PreFixTree.h
@@ -0,0 +1,29 @@
+#include <unordered_map>
+#include <string>
+#ifndef PREFIX_TREE_H
+#define PREFIX_TREE_H
+
+struct PTNode{
+	bool isWord;
+	int count;
+	std::unordered_map<char, PTNode*> children;
+	double latitude;
+	double longitude;
+	
+};
+
+class Prefix_Tree{
+	private:
+		PTNode *root;
+
+
+	public:
+		Prefix_Tree();
+		~Prefix_Tree();
+		void insert_word(std::string word, double latitude, double longitude);
+		int find_word(std::string word, double &latitude, double &longitude);
+		
+
+};
+
+#endif
